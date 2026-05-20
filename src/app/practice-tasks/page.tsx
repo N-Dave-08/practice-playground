4644c6c25b575b4acc7943f34d156e1b5b3b12d7
N-Dave-08 <cdave.dev@gmail.com>
2026-05-21 04:32:51 +0800

    feat: add tasks for filtering active users and grouping by role with examples

--- src/app/practice-tasks/page.tsx
+++ src/app/practice-tasks/page.tsx
@@ -101,6 +101,103 @@ const count: CountMap = fruits.reduce(
 
 console.log("count occurrences", count);
 
+// ================================================
+//   Task 1
+// ================================================
+
+// 🧩 Problem:
+
+// You are given an array of users from an API.
+
+// Your task is to:
+
+// ✅ 1. Filter only active users
+// ✅ 2. Return only their emails
+// ✅ 3. Group emails by domain (gmail.com, yahoo.com, etc.)
+
+{
+  type User = {
+    id: number;
+    name: string;
+    email: string;
+    active: boolean;
+  };
+
+  const users: User[] = [
+    { id: 1, name: "A", email: "a@gmail.com", active: true },
+    { id: 2, name: "B", email: "b@yahoo.com", active: false },
+    { id: 3, name: "C", email: "c@gmail.com", active: true },
+    { id: 4, name: "D", email: "d@outlook.com", active: true },
+    { id: 5, name: "E", email: "e@yahoo.com", active: true },
+  ];
+
+  const filteredUsers = users
+    .filter((user: User) => user.active)
+    .reduce((acc: Record<string, string[]>, user: User) => {
+      // extract domain from email
+      const domain = user.email.split("@")[1];
+
+      // initialize array if not exists
+      if (!acc[domain]) {
+        acc[domain] = [];
+      }
+
+      // push email into correct domain group
+      acc[domain].push(user.email);
+
+      return acc;
+    }, {});
+
+  console.log(filteredUsers);
+}
+
+// ================================================
+//   Task 2
+// ================================================
+
+// 🧩 Problem:
+
+// This function is supposed to:
+
+// Group active users by role and return an object like:
+
+// {
+//   admin: ["Alice"],
+//   user: ["Bob"]
+// }
+
+// But it is currently broken.
+
+{
+  type User = {
+    name: string;
+    role: string;
+    active: boolean;
+  };
+
+  const users: User[] = [
+    { name: "Alice", role: "admin", active: true },
+    { name: "Bob", role: "user", active: true },
+    { name: "Charlie", role: "user", active: false },
+  ];
+
+  function groupUsers(users: User[]) {
+    return users.reduce((acc: Record<string, string[]>, user: User) => {
+      if (!user.active) return acc;
+
+      if (!acc[user.role]) {
+        acc[user.role] = [];
+      }
+
+      acc[user.role].push(user.name);
+
+      return acc;
+    }, {});
+  }
+
+  console.log(groupUsers(users));
+}
+
 export default function PracticeTask() {
   return <div>page</div>;
 }
